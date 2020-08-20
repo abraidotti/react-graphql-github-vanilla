789b357101cd09f1f8c46b7c4fce7b9ca898ec22
Alessandro Braidotti <alessandro@kintyre.co>
2020-08-20 11:29:57 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # react-graphql-github-vanilla
+
 A tutorial app that queries Github's v4 (GraphQL) API
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+const axiosGitHubGraphQL = axios.create({
+  baseURL: "https://api.github.com/graphql",
+  headers: {
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+  },
+});
+
+const TITLE = "React GraphQL GitHub Client";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{TITLE}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
